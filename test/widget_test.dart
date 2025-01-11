6b1adfa82994a8cca4c773a4f2112c65f58a8f34
EstebanGitPro <estebanpoly.e.a@gmail.com>
2025-01-11 09:26:30 -0500

    clone husky

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,6 +14,6 @@ void main() {
     await tester.pumpWidget(const MyApp());
 
     // Verify that our text is displayed
-    expect(find.text('wennas Actions! and husk'), findsOneWidget);
+    expect(find.text('wennas Actions! and husk clone'), findsOneWidget);
   });
 }
