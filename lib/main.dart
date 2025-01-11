bfe8f6561ca1bd8844162d35c5814b070e79a00f
EstebanGitPro <estebanpoly.e.a@gmail.com>
2025-01-11 09:59:38 -0500

    test: add commit-msg

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Text('wennas Actions! and husk clone'),
+      home: Text('wennas Actions! and husk clone whit commit-msg'),
     );
   }
 }
