819a87c4cb9706788d43dce6ac818d224c426909
EstebanGitPro <estebanpoly.e.a@gmail.com>
2025-01-11 08:58:15 -0500

    refactor: document pre-commit husk

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,7 +14,6 @@ void main() {
     await tester.pumpWidget(const MyApp());
 
     // Verify that our text is displayed
-    expect(
-        find.text('wennas!! Actions! and gradel ok and husk'), findsOneWidget);
+    expect(find.text('wennas!! Actions! and husk'), findsOneWidget);
   });
 }
